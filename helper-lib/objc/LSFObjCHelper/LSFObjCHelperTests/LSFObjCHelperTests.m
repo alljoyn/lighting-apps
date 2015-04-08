a99cfe466253e0f1c10719d7cc450ed8df9a1a13
David Diplock <ddiplock@qce.qualcomm.com>
2015-04-08 13:47:08 -0700

    Replace AllSeenAlliance License with Apache License Version 2.0
    http://www.apache.org/licenses/LICENSE-2.0
    
    * Executed in batch by
      https://cgit.allseenalliance.org/extras/automation.git/tree/relicense/bin/do-relicense
    * Patch review took place here:
      https://git.allseenalliance.org/gerrit/#/c/9377/
    
    * For each branch of each project of the AllJoyn Open Source Project
      gerrit repository
    - search / replace old license with new in all matching files
    - search / replace old copyright with new in all files which only
      include copyright
    - assign copyright to Open Connectivity Foundation, AllJoyn Open
      Source Project Contributors and others.
    - propose changes for review by commiters
    - include SPDX
    
    Change-Id: I797965a6398a85449dcec816f1e4c8cc9d57f9da
    Signed-off-by: C.J. Collier <cjcollier@linuxfoundation.org>

--- helper-lib/objc/LSFObjCHelper/LSFObjCHelperTests/LSFObjCHelperTests.m
+++ helper-lib/objc/LSFObjCHelper/LSFObjCHelperTests/LSFObjCHelperTests.m
@@ -3,7 +3,7 @@
 //  LSFObjCHelperTests
 //
 //  Created by Fullen, Jason on 9/9/14.
-//  Copyright (c) 2014 AllSeen Alliance. All rights reserved.
+//  Copyright (c) Connectivity Foundation and AllJoyn Open Source Project Contributors and others.
 //
 
 #import <XCTest/XCTest.h>
